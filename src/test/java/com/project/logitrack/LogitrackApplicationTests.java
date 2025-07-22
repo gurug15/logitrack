cd4f68f2f832c6f4b3133e628bfc8580488bc044
GURUGASTGAR <gurugastgar@gmail.com>
2025-07-22 22:57:38 +0530

    started with logitrack project

---
+++ src/test/java/com/project/logitrack/LogitrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.logitrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogitrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
